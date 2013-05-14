f32d1242c7af9dd183ffe6e3d34a7631c0558721
david122886 <liuchong1228@yahoo.cn>
2013-05-14 15:14:19 +0800

    Initial Commit

---
+++ PhotoUploadTool/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PhotoUploadTool
+//
+//  Created by david on 13-5-14.
+//  Copyright (c) 2013年 Comdosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

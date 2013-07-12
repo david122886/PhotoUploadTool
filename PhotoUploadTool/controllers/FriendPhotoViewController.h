1ef01f84294df2b7ba9e929dfc43079e8b3f2fc2
david.liu <liuchong1228@yahoo.cn>
2013-07-12 18:54:51 +0800

    finished seach the friends at the same city

---
+++ PhotoUploadTool/controllers/FriendPhotoViewController.h
@@ -0,0 +1,13 @@
+//
+//  FriendPhotoViewController.h
+//  PhotoUploadTool
+//
+//  Created by david on 13-7-11.
+//  Copyright (c) 2013年 Comdosoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendPhotoViewController : UIViewController
+
+@end
---
+++ PhotoUploadTool/controllers/FriendPhotoViewController.m
@@ -0,0 +1,38 @@
+//
+//  FriendPhotoViewController.m
+//  PhotoUploadTool
+//
+//  Created by david on 13-7-11.
+//  Copyright (c) 2013年 Comdosoft. All rights reserved.
+//
+
+#import "FriendPhotoViewController.h"
+
+@interface FriendPhotoViewController ()
+
+@end
+
+@implementation FriendPhotoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
